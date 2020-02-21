14cc3ddcda9e5402de88ca87121b816ff2a24e37
mshanemc <shane.mclaughlin@salesforce.com>
2020-02-21 10:17:27 -0600

    flexi stuff

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
 sfdx force:apex:execute -f scripts/apex/hello.apex 
-sfdx force:org:open -r /lightning/o/Contact/list?filterName=All_Contacts
+sfdx force:org:open -p /lightning/o/Contact/list?filterName=All_Contacts
